8aee75140ef8bf27375f36cc695feed2c9895a7b
Albert Tazetdinov <zet565758@gmail.com>
2023-08-04 11:21:36 +0300

    тест

---
+++ dist/index.a71c2874.js
@@ -0,0 +1,25 @@
+const wrapper = document.querySelector(".content");
+const layers = document.querySelectorAll(".parallax__img");
+const handleParallax = (evt)=>{
+    //размер области просмотра
+    const parallaxLeftOffset = wrapper.getBoundingClientRect().left;
+    const parallaxTopOffset = wrapper.getBoundingClientRect().top;
+    // координаты центра экрана
+    const coordX = evt.clientX - parallaxLeftOffset - 0.5 * wrapper.offsetWidth;
+    const coordY = evt.clientY - parallaxTopOffset - 0.5 * wrapper.offsetHeight;
+    layers.forEach((layer)=>{
+        const layerSpeed = layer.dataset.speed;
+        const x = coordX.toFixed(2);
+        const y = coordY.toFixed(2);
+        layer.setAttribute("style", `transform: translate(${x}px, ${y}px);`);
+    });
+};
+const reset = ()=>{
+    layers.forEach((layer)=>{
+        layer.removeAttribute("style");
+    });
+};
+wrapper.addEventListener("mousemove", handleParallax);
+wrapper.addEventListener("mouseout", reset);
+
+//# sourceMappingURL=index.a71c2874.js.map
